6be5c760703c673b9926d6b125652e4a0684a53f
Jon Bates <jonathan.bates@team.bumble.com>
2021-01-25 15:18:56 +0000

    Move to helm chart for prometheus deployment

--- init.sh
+++ init.sh
@@ -1,14 +1,21 @@
-kubectx docker-desktop
+#kubectx docker-desktop
 
 # Apps
+echo "Deploying apps..."
 kubectl create namespace apps
 kubectl -n apps apply -f noddyapp.yaml
+echo "Deploying apps - done!"
 
-# Prometheus
-kubectl apply -f operators.yaml
+# Monitoring
+echo "Getting ready to deploy monitoring stack..."
 kubectl create namespace monitoring
-kubectl apply -n monitoring -f prometheus-rbac.yaml
-kubectl apply -n monitoring -f prometheus.yaml
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo update
+echo "Getting ready to deploy monitoring stack - done!"
+
+echo "Deploying the monitoring stack..."
+helm install -f values.yaml prometheus-stack prometheus-community/kube-prometheus-stack
+echo "Deploying the monitoring stack - done!"
 
 # Dashboard
 kubectl create namespace kubernetes-dashboard
@@ -19,4 +26,10 @@ kubectl -n kubernetes-dashboard get secret $(kubectl -n kubernetes-dashboard get
 
 echo "The k8s token has been copied to your pasteboard"
 kubectl proxy &
+
+echo "small pause before opening services..."
+sleep 5
+
 open http://localhost:8001/api/v1/namespaces/kubernetes-dashboard/services/https:kubernetes-dashboard:/proxy/
+open http://localhost:30090
+open http://localhost:30903
